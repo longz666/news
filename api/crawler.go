44c5d31c9aee92976059744514a75a0be2fe94f2
longz666 <162688466+longz666@users.noreply.github.com>
2024-05-04 18:16:59 +0800

    Update crawler.go

--- api/crawler.go
+++ api/crawler.go
@@ -2,8 +2,8 @@ package api
 
 import (
 	"encoding/json"
-	"github.com/codecodify/news/lib"
-	"github.com/codecodify/news/lib/impl"
+	"github.com/longz666/news/lib"
+	"github.com/longz666/news/lib/impl"
 	"log"
 	"net/http"
 	"strconv"
